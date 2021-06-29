7b8181dad67d2f43897cd1110412513acdad93a3
Deep Pudasaini <deep.pudasaini19@my.northampton.ac.uk>
2021-06-29 13:09:10 +0545

    Templates Added

--- Blog/Home/urls.py
+++ Blog/Home/urls.py
@@ -2,5 +2,12 @@ from django.contrib import admin
 from django.urls import path,include
 from . import views
 urlpatterns = [
-path('',views.Index,name="Home Page")
+path('',views.Index,name="Home Page"),
+path('home',views.home,name="Home"),
+path('login',views.Login,name="Login"),
+path('contact',views.Contact,name="Contact"),
+path('about',views.About,name="About"),
+path('post',views.SinglePost,name="Post"),
+
+
 ]
--- Blog/Home/views.py
+++ Blog/Home/views.py
@@ -2,4 +2,14 @@ from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 def Index(request):
-    return HttpResponse("Ola")
+    return render(request,'index.html')
+def About(request):
+    return render(request,'about.html')    
+def Contact(request):
+    return render(request,'contact.html')
+def SinglePost(request):
+    return render(request,'post.html')
+def Login(request):
+    return render(request,'login.html')
+def home(request):
+    return render(request,'index.html')
